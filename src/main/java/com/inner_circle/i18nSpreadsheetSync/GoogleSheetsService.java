2a4a4d08a116df88bf6462a5010331994ba7cb68
Mason <donghyeonlee@megazone.com>
2024-08-08 17:13:16 +0900

    fix: 에러수정

--- src/main/java/com/inner_circle/i18nSpreadsheetSync/GoogleSheetsService.java
+++ src/main/java/com/inner_circle/i18nSpreadsheetSync/GoogleSheetsService.java
@@ -4,7 +4,6 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
@@ -13,13 +12,11 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,22 +25,28 @@ public class GoogleSheetsService {
     private final GoogleSheetsProperties properties;
     private Sheets sheetsService;
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private final ResourceLoader resourceLoader;
 
 
     @Autowired
-    public GoogleSheetsService(GoogleSheetsProperties properties) {
+    public GoogleSheetsService(GoogleSheetsProperties properties, ResourceLoader resourceLoader) {
         this.properties = properties;
+        this.resourceLoader = resourceLoader;
+
     }
 
     private Sheets getSheetsService() throws GeneralSecurityException, IOException {
         if (sheetsService == null) {
             GoogleCredentials credentials;
-            try (var serviceAccountStream = new ClassPathResource(
-                properties.getServiceAccountKeyPath()).getInputStream()) {
+            Resource resource = resourceLoader.getResource(properties.getServiceAccountKeyPath());
+
+            try (var serviceAccountStream = resource.getInputStream()) {
                 credentials = ServiceAccountCredentials.fromStream(serviceAccountStream)
                     .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+            } catch (IOException e) {
+                System.err.println("Error loading service account key: " + e.getMessage());
+                throw e;
             }
-
             HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
 
             sheetsService = new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
